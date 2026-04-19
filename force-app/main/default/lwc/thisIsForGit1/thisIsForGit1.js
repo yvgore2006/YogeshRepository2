2cfe07629f3285488791fbcb1f534fd2cae98824
Yogesh <yogeshvalmikgore@gmail.com>
2026-04-19 07:21:50 +0530

    LWC is Modified

--- force-app/main/default/lwc/thisIsForGit1/thisIsForGit1.js
+++ force-app/main/default/lwc/thisIsForGit1/thisIsForGit1.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class ThisIsForGit1 extends LightningElement {}
+export default class ThisIsForGit1 extends LightningElement {
+
+    handleClick() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'Welcome to the new world',
+            variant: 'success'
+        });
+
+        this.dispatchEvent(event);
+    }
+
+}
